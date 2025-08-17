a14969f0b6319b812223980c3c11a4dccb73d515
Mikey Dowsett <mikeydowsett@proton.me>
2025-08-17 13:53:47 -0500

    CORS Update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 app.include_router(stripe_api.stripe_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://loftly.fly.dev"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
